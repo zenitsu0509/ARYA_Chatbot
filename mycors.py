9eac62e35e18d49050b2a1380c9b05689e7911ad
hackerX400 <gangwarsanu8@gmail.com>
2024-10-16 12:41:16 +0530

    fix streamlit app

--- mycors.py
+++ mycors.py
@@ -1,5 +1,4 @@
 import os
-from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from langchain_pinecone import PineconeVectorStore
 from langchain_pinecone import PineconeEmbeddings
@@ -11,23 +10,15 @@ from flask_cors import CORS
 
 load_dotenv()
 
-app = Flask(__name__)
-CORS(app)
-
-
-
 pinecone_api_key = os.getenv('PINECONE_API_KEY')
 pinecone_environment = os.getenv('PINECONE_ENV')
 pc = Pinecone(api_key=pinecone_api_key, environment=pinecone_environment)
 
-
 index_name = "arya-index"
 index = pc.Index(index_name)
 
-
 model_name = "intfloat/multilingual-e5-large"
 embeddings = HuggingFaceEmbeddings(model_name=model_name)
-
 
 docsearch = PineconeVectorStore(
     index=index,
@@ -47,19 +38,6 @@ llm = HuggingFaceEndpoint(
 )
 
 
-template = """
-You are Arya, the official bot of Arya Bhatt Hostel. Humans will ask you questions about the Arya Bhatt Hostel. 
-Use the following context from the vector database to answer the question.
-If you don't know the answer, just say you don't know.
-Answer in a short and concise way.
-
-Question: {question}
-Answer:
-"""
-
-prompt = PromptTemplate(template=template, input_variables=["question"])
-
-
 qa_chain = RetrievalQA.from_chain_type(
     llm=llm,
     chain_type="stuff",
@@ -67,23 +45,9 @@ qa_chain = RetrievalQA.from_chain_type(
     return_source_documents=True
 )
 
-@app.route('/chat', methods=['POST', 'OPTIONS'])
-def chat():
-    if request.method == 'OPTIONS':
-        # Preflight request handling
-        return jsonify({"response": "CORS preflight passed."}), 200
-    
-    data = request.json
-    user_question = data.get('question', '')
-    
-    if user_question:
-        # Get the response from the QA model
-        response = qa_chain.invoke(user_question)
-        chatbot_response = response['result']
-        
-        return jsonify({"response": chatbot_response})
-    
-    return jsonify({"response": "Please provide a valid question."})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def give_my_response(user_question):
+    response = qa_chain.invoke(user_question)
+    chatbot_response = response['result']
+    return chatbot_response
+    
