89b8e82bf79e31dfea538ee85ff22d051beec1b5
Himanshu Gangwar <151658698+zenitsu0509@users.noreply.github.com>
2024-10-22 08:36:29 +0530

    Delete mess_test.py

--- mess_test.py
+++
@@ -1,8 +0,0 @@
-from mess_menu import get_mess_menu
-
-# In your chatbot's message handler
-def handle_message(message):
-    if "menu" in message.lower():
-        response = get_mess_menu()
-        # Send response back to user
-        print(response)  # or however you send messages in your chatbot
